a888cf1cbbb15844583943b61be226aaddf44060
burnst100 <burnst100@gmail.com>
2013-05-22 06:36:34 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo htc_vigor-userdebug
-add_lunch_combo htc_vigor-eng
+add_lunch_combo liquid_vigor-userdebug
+
